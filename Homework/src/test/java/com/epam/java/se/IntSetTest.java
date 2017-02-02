7b76963b5a36a4c27adc82ef887e0595d5e06c3c
dimatrifonov <dtrifonov@hotmail.com>
2017-02-02 15:33:54 +0300

    Add files via upload

--- Homework/src/test/java/com/epam/java/se/IntSetTest.java
+++ Homework/src/test/java/com/epam/java/se/IntSetTest.java
@@ -7,16 +7,56 @@ import static org.junit.Assert.*;
  */
 public class IntSetTest {
     @Test
-    public void add () throws Exception{
+    public void add () throws Exception {
         final IntSet set = new IntSet();
 
         assertFalse (set.contains(0));
         set.add(0);
         assertTrue (set.contains(0));
+
         assertFalse (set.contains(5));
+
+        set.add(100);
+        assertTrue (set.contains(100));
+        assertFalse (set.contains(101));
+
+
 
     }
 
+    @Test
+    public void remove () throws Exception {
+        final IntSet set = new IntSet();
 
+        assertFalse (set.contains(1));
+        set.add(1);
+        assertTrue(set.contains(1));
 
-}
+        set.remove(1);
+        assertFalse (set.contains(1));
+    }
+
+    @Test
+    public void contains() throws Exception {
+        final IntSet set = new IntSet();
+
+        for (int i=0; i<1000; i++) {
+            assertFalse(set.contains(i));
+        }
+
+        set.add(1);
+        set.add(20);
+        set.add(80);
+
+        for (int i=0; i<1000; i++) {
+            if (i==1 || i==20 || i==80) {
+                assertTrue(set.contains(i));
+            }
+                else {
+                    assertFalse(set.contains(i));
+                }
+
+            }
+        }
+
+    }
